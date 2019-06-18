c3dfcdf2843f15a5608c435fbf036e90087a89f4
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 04:59:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,7 @@ var destructivelyRemoveElementFromBeginningOfArray = function(array){
 var removeElementFromBeginningOfArray = function(array){
   return array.slice(1)
 }
+
+var destructivelyRemoveElementFromEndOfArray = function(array){
+  return array.pop()
+}
