2df9d080165098541aa81379023833d9f25afc23
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 04:21:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,6 @@ var addElementToBeginningOfArray = function(array, element){
 }
 
 var destructivelyAddElementToBeginningOfArray = function(array, element) {
-  var array = [element, ...array]
+  array.unshift(element)
   return array
 }
