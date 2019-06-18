5890f14f84382870a2bec1ac010170868d2a232e
Jun Liang <illuminatemynight@gmail.com>
2019-06-18 03:54:33 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles',
+];
+
+var addElementToBeginningOfArray = function(array, element){
+  
+  [element, ...array]
+  return array
+}
+
+var destructivelyAddElementToBeginningOfArray = function(array, element) {
+
+}
